54bd3dc2a41b89009ab3c4058f48cb4cf3fa1c4a
umang <umangsolanki591@gmail.com>
2023-07-18 11:45:51 +0530

    editerd

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,1 +1,110 @@
-<H1>LARAVEL FOOTER-PAGE</H1>
+
+<footer class=" text-white my-4 mb-0 bg-dark">
+        <div class="container-fluid text-md-left bg-darkblack" style="background: #010101">
+        <hr class="bottam_hr">
+        <div class="class row text-md-left">
+          <div class="col-md-3 col-lg-3 col-xl-3 col-auto mt-3">
+          <div class="logo mb-4">#</div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum vitae accusantium commodi sunt aliquam sapiente tenetur ex eligendi est animi neque, explicabo, recusandae officiis odio magnam asperiores rem reiciendis similique!</p>
+      </div>
+      
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h5 class="text-uppercase mb-4 font-weight-bold "> Useful Links</h5>
+              <p>
+                <a href="index.php" style="text-decoration:none;">Home</a>
+              </p>
+              <p>
+                <a href="feedback.php" style="text-decoration:none;">Contact-us</a>
+              </p>
+              <p>
+                <a href="aboutus.php" style="text-decoration:none;">About</a>
+              </p>
+
+              
+            </div>
+            
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h5 class="text-uppercase mb-4 font-weight-bold">SERVICES</h5>
+              <p>
+                <a href="bat_product.php" style="text-decoration:none;">Lorem ipsum.</a>
+              </p>
+              <p>
+                <a href="ball_product.php"  style="text-decoration:none;">Lorem, ipsum.</a>
+              </p>
+              <p>
+                <a href="bags_product.php" style="text-decoration:none;">Lorem, ipsum.</a>
+              </p>
+              <p>
+                <a href="apparels_product.php"  style="text-decoration:none;">Lorem, ipsum.</a>
+              </p>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 m-auto mt-3">
+              <h5 class="text-uppercase mb-4">Contact</h5>
+              <p>
+                <i class="fas fa-home mr-3"></i> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, a?
+              </p>
+              <p>
+                <i class="fas fa-envelope mr-3"></i>Lorem ipsum dolor sit.
+              </p>
+              <p>
+                <i class="fas fa-phone mr-3"></i> +91 ***********
+              </p>
+              <p>
+                <i class="fas fa-phone mr-3"></i> +91 ******
+              </p>
+            </div>
+          </div>
+          
+          <hr class="mb-4 m-0">
+          <div class="row d-flex justify-content-center">
+            <div >
+              <p class="text-center">Copyright ©<?php echo date("Y");?> FASC | All Rights Reserved | Developed by<a href="#" class="text-primary" style="text-decoration: none;"> U M A N G</a></p>
+             
+              <div class="row">
+                <div class="text-center text-md-right">
+                  <div class="list-unstyled inline-flex">
+                  <ul class="list-unstyled inline-flex">
+                     
+                    <a href="#" class="btn-floating btn-sm" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
+                     
+                    <a href="#" target="_blank" class="btn-floating btn-sm text-danger" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
+                   
+                    <a href="#" class="btn-floating btn-sm" style="font-size: 23px;"><i class="fab fa-linkedin-in"></i></a>
+                    
+                    <a href="#" class="btn-floating btn-sm text-danger" style="font-size: 23px;"><i class="fab fa-youtube"></i></a>
+  
+                    <a href="#" class="btn-floating btn-sm" style="font-size: 23px;"><i class="fab fa-twitter"></i></a>
+                   
+                    </ul>
+  
+                    <div id="backtotop"><a href="#"></a></div>
+            
+                    <button>
+                        <div class="text">
+                            <span>Back</span>
+                            <span>to</span>
+                            <span>top</span>
+                        </div>
+                        <div class="clone"><a href="#">
+                            <span>Back</span>
+                            <span>to</span>
+                            <span>top</span> </a>
+                        </div>
+                        <svg width="20px" xmlns="#" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                        </svg>
+                    </button>
+            </div>
+                </div>
+                
+              </div>
+  
+            </div>
+            </div>
+
+          </div>
+        </div>
+    </footer>
+
+  
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,59 +1,67 @@
-@include('hearder')
 
-
-<h1>Laravel-project</h1>
-<a href="/hey">post page</a>
-{{ 5 + 2}}
-<br><br>
-
-{!!"<h1>Laravel</h1>"!!}
-{{-- {!!"<script>alert('are you ready..')</script>"!!} --}}
-@php
-    $name = ["meet","jitu","vishal","umang"]
-@endphp
-<ul>
-@foreach ($name as $n )
-<li>{{ $loop->index }} - {{ $n }}</li>
-{{-- <li>{{ $loop->iteration}} - {{ $n }}</li> --}}
-
-@endforeach
-</ul>
-<ul>
-    @foreach ($name as $n )
-    {{-- <li>{{ $loop->index }} - {{ $n }}</li> --}}
-    <li>{{ $loop->iteration}} - {{ $n }}</li>
-    
-    @endforeach
-    </ul>
-    <ul>
-        @foreach ($name as $n )
-        <li>{{ $loop->count }} - {{ $n }}</li>
-        {{-- <li>{{ $loop->iteration}} - {{ $n }}</li> --}}
-        
-        @endforeach
-        </ul>
-
-        <ul>
-            @foreach ($name as $n )
-            @if ($loop->first)
-            <li style="color:red">{{ $n }}</li>
-          @elseif ($loop->last)
-          <li style="color:rgb(217, 255, 0)">{{ $n }}</li>      
-                @else
-                <li style="color:lightpink">{{ $n }}</li>  
-            @endif      
-            @endforeach
-            </ul>
-            <ul>
-                @foreach ($name as $n )
-                @if ($loop->even)
-                <li style="color:red">{{ $n }}</li>
-              @elseif ($loop->odd)
-              <li style="color:rgb(217, 255, 0)">{{ $n }}</li>      
-                    @else
-                    <li style="color:lightpink">{{ $n }}</li>  
-                @endif      
-                @endforeach
-                </ul>
-               
+<head>
+    <link rel="icon" type="image/x-icon" href="img/pngwing.com (6).png">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!--Main Css-->
+      
+      <!--Font Awesome-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+      <!--Font Awesome-->
+      
+      <!--Google Font-->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+      <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Open+Sans:wght@500;800&family=Oswald:wght@300&family=Poppins:wght@300;400&family=Roboto+Condensed:wght@300&family=Urbanist:ital,wght@0,100;0,400;1,300&display=swap" rel="stylesheet">
+      <!--Font
+      font-family: 'Dancing Script', cursive;
+      font-family: 'Open Sans', sans-serif;
+      font-family: 'Oswald', sans-serif;
+      font-family: 'Poppins', sans-serif;
+      font-family: 'Roboto Condensed', sans-serif;
+      font-family: 'Urbanist', sans-serif;-->
+      <!--Google Font-->
+      
+      <!-- Bootstrap CSS -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  
+      
+      <!--Main Css-->
+      <link rel="stylesheet" href="mobile.css">
+      <link rel="stylesheet" href="all_products.css">
+      <link rel="stylesheet" href="css/styale.css">
+      <link rel="stylesheet" href="about.css">
+  
+  
+      <title>FASC</title>
+    </head>
+    @include('hearder')
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="{{ asset('/public/img/pexels-johannes-plenio-1642770 (1).jpg') }}" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{ asset('/public/img/pexels-quang-nguyen-vinh-2563129.jpg') }}" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{ asset('/public/img/pexels-quang-nguyen-vinh-2563129.jpg') }}" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+              
 @include('footer')                
